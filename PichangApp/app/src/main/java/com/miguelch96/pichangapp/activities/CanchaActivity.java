0f74a01d9554ebde52e047bd295ace91dc504d76
miguelch96 <miguelangelca96@outlook.com>
2017-09-19 18:17:32 -0500

    Pantalla Cancha v1.0

---
+++ PichangApp/app/src/main/java/com/miguelch96/pichangapp/activities/CanchaActivity.java
@@ -0,0 +1,15 @@
+package com.miguelch96.pichangapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.miguelch96.pichangapp.R;
+
+public class CanchaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancha);
+    }
+}
