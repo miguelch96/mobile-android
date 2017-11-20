03e39e4780e724ba5412f17e7478ee5e85d430b4
sergioalh06 <sergioalh0306@gmail.com>
2017-11-19 21:43:16 -0500

    try post reserva horariosDialog

--- PichangApp/app/src/main/java/com/miguelch96/pichangapp/dialogs/HorariosDialog.java
+++ PichangApp/app/src/main/java/com/miguelch96/pichangapp/dialogs/HorariosDialog.java
@@ -14,12 +14,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.miguelch96.pichangapp.R;
 import com.miguelch96.pichangapp.dialogs.equipo.ConfirmacionDialog;
 import com.miguelch96.pichangapp.models.Cancha;
 import com.miguelch96.pichangapp.models.Reserva;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -65,12 +73,15 @@ public class HorariosDialog extends DialogFragment {
 
                 Bundle bundle = getArguments();
                 String v = bundle.getString("object");
+                bundle.putString("horario",value);
+
                 if(v.equalsIgnoreCase("equipo")){
                     DialogFragment dialog = new ConfirmacionDialog();
                     dialog.setArguments(bundle);
                     dialog.show(getFragmentManager(), "ConfirmacionDialog");
                 }
                else if(v.equalsIgnoreCase("cancha")){
+                    //putReserva(bundle);
                     DialogFragment dialog = new MessageDialog();
                     String message = "!Reserva realizada con exito!";
                     bundle.putString("message", message);
@@ -95,6 +106,33 @@ public class HorariosDialog extends DialogFragment {
         builder.setView(view);
         AlertDialog alert = builder.create();
         return alert;
+    }
+
+    public void putReserva(Bundle bundle){
+        Cancha c =(Cancha) bundle.getSerializable("cancha");
+        String dia = bundle.getString("dia");
+        String horario = bundle.getString("horario");
+        String fecha =  String.valueOf(Calendar.getInstance().getFirstDayOfWeek());
+
+        AndroidNetworking.post("https://fierce-cove-29863.herokuapp.com/createAnUser")
+                .addBodyParameter("dia", dia)
+                .addBodyParameter("horario", horario)
+                .addBodyParameter("fecha", fecha)
+                .addBodyParameter("idCancha", String.valueOf(c.getId()))
+                .addBodyParameter("idUsuario", String.valueOf(1))
+                .setTag("test")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                    }
+                });
     }
 
     @Nullable
